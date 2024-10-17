b14ed74ac153f611d7f06de150c206fd39a59ca5
Sanchi7412 <yuusuke7412@gmail.com>
2024-10-17 06:21:20 +0000

    ollama

--- src/index.ts
+++ src/index.ts
@@ -4,38 +4,53 @@ import * as z from 'zod';
 import { generate } from '@genkit-ai/ai';
 import { configureGenkit } from '@genkit-ai/core';
 import { defineFlow, startFlowsServer } from '@genkit-ai/flow';
-import { googleAI } from '@genkit-ai/googleai';
-
-// Import models from the Google AI plugin. The Google AI API provides access to
-// several generative models. Here, we import Gemini 1.5 Flash.
+import { ollama } from 'genkitx-ollama';
 
 configureGenkit({
   plugins: [
     ollama({
-      models: [{ name: 'gemma2:27b' }],
-      serverAddress: 'http://100.118.110.78:11434',
+      // Ollama provides an interface to many open generative models. Here,
+      // we specify Google's Gemma model. The models you specify must already be
+      // downloaded and available to the Ollama server.
+      models: [{ name: 'gemma' }],
+      // The address of your Ollama API server. This is often a different host
+      // from your app backend (which runs Genkit), in order to run Ollama on
+      // a GPU-accelerated machine.
+      serverAddress: 'http://127.0.0.1:11434',
     }),
   ],
+  // Log debug output to tbe console.
   logLevel: 'debug',
+  // Perform OpenTelemetry instrumentation and enable trace collection.
   enableTracingAndMetrics: true,
-})
+});
 
+// Define a simple flow that prompts an LLM to generate menu suggestions.
 export const menuSuggestionFlow = defineFlow(
-    {
-      name: 'menuSuggestionFlow',
-      inputSchema: z.string(),
-      outputSchema: z.string(),
-    },
-    async subject => {
-      const llmResponse = await generate({
-        prompt: `Suggest an item for the menu of a ${subject} themed restaurant`,
-        model: 'ollama/gemma2:27b',
-        config: {
-          temperature: 1,
-        },
-      })
-      return llmResponse.text()
-    }
-)
+  {
+    name: 'menuSuggestionFlow',
+    inputSchema: z.string(),
+    outputSchema: z.string(),
+  },
+  async (subject) => {
+		// Construct a request and send it to the model API.
+    const llmResponse = await generate({
+      prompt: `Suggest an item for the menu of a ${subject} themed restaurant`,
+      model: 'ollama/gemma',
+      config: {
+        temperature: 1,
+      },
+    });
 
-startFlowsServer()
+		// Handle the response from the model API. In this sample, we just convert
+    // it to a string, but more complicated flows might coerce the response into
+    // structured output or chain the response into another LLM call, etc.
+    return llmResponse.text();
+  }
+);
+
+// Start a flow server, which exposes your flows as HTTP endpoints. This call
+// must come last, after all of your plug-in configuration and flow definitions.
+// You can optionally specify a subset of flows to serve, and configure some
+// HTTP server options, but by default, the flow server serves all defined flows.
+startFlowsServer();
