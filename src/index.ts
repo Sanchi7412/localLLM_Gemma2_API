0db35c31504eb304a96da9f6f2d599e4d47f71e5
Yuusuke Ishii <111514756+Sanchi7412@users.noreply.github.com>
2024-10-18 19:23:01 +0900

    陽気な子AI テスト

--- src/index.ts
+++ src/index.ts
@@ -7,54 +7,54 @@ import { defineFlow, startFlowsServer } from '@genkit-ai/flow';
 import { ollama } from 'genkitx-ollama';
 
 configureGenkit({
-  plugins: [
-    ollama({
-      models: [{ name: 'gemma2:27b' }],
-      // serverAddress: 'http://127.0.0.1:11434',
-      serverAddress: 'http://100.118.110.78:11434',
-    }),
-  ],
-  logLevel: 'debug',
-  enableTracingAndMetrics: true,
+	plugins: [
+		ollama({
+			models: [{ name: 'gemma2:27b' }],
+			serverAddress: 'http://127.0.0.1:11434',
+			// serverAddress: 'http://100.118.110.78:11434',
+		}),
+	],
+	logLevel: 'debug',
+	enableTracingAndMetrics: true,
 });
 
 // Define a simple flow that prompts an LLM to generate menu suggestions.
 export const testPrompt = defineFlow(
-  {
-    name: 'テスト',
-    inputSchema: z.string(),
-    outputSchema: z.string(),
-  },
-  async (subject) => {
-    const llmResponse = await generate({
-      prompt: `${subject}`,
-      model: 'ollama/gemma2:27b',
-      config: {
-        temperature: 1,
-      },
-    });
+	{
+		name: 'テスト',
+		inputSchema: z.string(),
+		outputSchema: z.string(),
+	},
+	async (subject) => {
+		const llmResponse = await generate({
+			prompt: `${subject}`,
+			model: 'ollama/gemma2:27b',
+			config: {
+				temperature: 1,
+			},
+		});
 
-    return llmResponse.text();
-  }
+		return llmResponse.text();
+	}
 );
 
 export const boy_prompt = defineFlow(
-    {
-      name: '陽気な子供',
-      inputSchema: z.string(),
-      outputSchema: z.string(),
-    },
-    async (subject) => {
-      const llmResponse = await generate({
-        prompt: `${subject}`,
-        model: 'ollama/gemma2:27b',
-        config: {
-          temperature: 1,
-        },
-      });
+	{
+		name: '陽気な子供',
+		inputSchema: z.string(),
+		outputSchema: z.string(),
+	},
+	async (subject) => {
+		const llmResponse = await generate({
+			prompt: `${subject}`,
+			model: 'ollama/gemma2:27b',
+			config: {
+				temperature: 1,
+			},
+		});
 
-      return llmResponse.text();
-    }
+		return llmResponse.text();
+	}
 );
 
 startFlowsServer();
