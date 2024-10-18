8ba20a3e1fa58f92a4bab8e794b1d98ad9572150
Yuusuke Ishii <111514756+Sanchi7412@users.noreply.github.com>
2024-10-18 19:23:38 +0900

    restore

--- src/index.ts
+++ src/index.ts
@@ -4,35 +4,53 @@ import * as z from 'zod';
 import { generate } from '@genkit-ai/ai';
 import { configureGenkit } from '@genkit-ai/core';
 import { defineFlow, startFlowsServer } from '@genkit-ai/flow';
-import { ollama } from 'genkitx-ollama';
+import { googleAI } from '@genkit-ai/googleai';
+
+// Import models from the Google AI plugin. The Google AI API provides access to
+// several generative models. Here, we import Gemini 1.5 Flash.
 
 configureGenkit({
 	plugins: [
 		ollama({
 			models: [{ name: 'gemma2:27b' }],
+<<<<<<< Updated upstream
 			serverAddress: 'http://127.0.0.1:11434',
 			// serverAddress: 'http://100.118.110.78:11434',
+=======
+			serverAddress: 'http://100.118.110.78:11434',
+>>>>>>> Stashed changes
 		}),
 	],
 	logLevel: 'debug',
 	enableTracingAndMetrics: true,
 });
 
+<<<<<<< Updated upstream
 // Define a simple flow that prompts an LLM to generate menu suggestions.
 export const testPrompt = defineFlow(
 	{
 		name: 'テスト',
+=======
+export const menuSuggestionFlow = defineFlow(
+	{
+		name: 'menuSuggestionFlow',
+>>>>>>> Stashed changes
 		inputSchema: z.string(),
 		outputSchema: z.string(),
 	},
 	async (subject) => {
 		const llmResponse = await generate({
+<<<<<<< Updated upstream
 			prompt: `${subject}`,
+=======
+			prompt: `Suggest an item for the menu of a ${subject} themed restaurant`,
+>>>>>>> Stashed changes
 			model: 'ollama/gemma2:27b',
 			config: {
 				temperature: 1,
 			},
 		});
+<<<<<<< Updated upstream
 
 		return llmResponse.text();
 	}
@@ -53,6 +71,8 @@ export const boy_prompt = defineFlow(
 			},
 		});
 
+=======
+>>>>>>> Stashed changes
 		return llmResponse.text();
 	}
 );
